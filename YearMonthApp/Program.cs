e261b31cd3f17a793b798d09fb53f8e85f36b964
A.Mahidwei <xile.meko@gmail.com>
2024-07-23 02:01:02 +0200

    emoving nested loop and using dictionary for more preformance

--- YearMonthApp/Program.cs
+++ YearMonthApp/Program.cs
@@ -5,21 +5,21 @@ public class Program
     public static void Main(string[] args)
     {
         DateTime dt1 = new DateTime(2020, 06, 23);
-        DateTime dt2 = new DateTime(2024,8,29);
+        DateTime dt2 = new DateTime(2024, 8, 29);
 
-       var yearMonths = YearMonth.GetYearWithListMonts(dt1,dt2);
+        var yearMonths = YearMonth.GetYearWithListMonths(dt1, dt2);
 
-        Console.WriteLine("Firest Date = {0}",dt1.Date);
+        Console.WriteLine("First Date = {0}", dt1.Date);
         Console.WriteLine();
-        Console.WriteLine("Secound Date = {0}",dt2.Date);
+        Console.WriteLine("Second Date = {0}", dt2.Date);
         Console.WriteLine();
         Console.WriteLine("=============================================================");
-        foreach (YearMonth yearMonth in yearMonths)
+        foreach (var kvp in yearMonths)
         {
-            Console.WriteLine(" Year => {0} ",yearMonth.Year);
-            foreach (int month in yearMonth.Months)
+            Console.WriteLine($"Year => {kvp.Key}");
+            foreach (var month in kvp.Value)
             {
-                Console.Write(" {0} ", month);
+                Console.Write($"{month} ");
             }
             Console.WriteLine();
         }
